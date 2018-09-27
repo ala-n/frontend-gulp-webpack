cff2ab3b04f7893d18b267a5f0361bd98a7da95d
yadamska <yuli.adamskaya@gmail.com>
2018-09-27 14:02:41 +0300

    main html

--- paths/paths-hbs.js
+++ paths/paths-hbs.js
@@ -3,4 +3,6 @@ const DIR_NAME_COMPONENTS = __dirname + '../../src/components/';
 module.exports.pathsArray = [
     DIR_NAME_COMPONENTS + 'main-page/main',
     DIR_NAME_COMPONENTS + 'main-page/component-news',
+    DIR_NAME_COMPONENTS + 'main-page/footer',
+    DIR_NAME_COMPONENTS + 'main-page/header',
 ];
