bd82560369ff71f3e67c7d7f260b6d91ea99d925
yadamska <yuli.adamskaya@gmail.com>
2018-09-28 12:50:06 +0300

    styles

--- paths/paths-hbs.js
+++ paths/paths-hbs.js
@@ -5,4 +5,5 @@ module.exports.pathsArray = [
     DIR_NAME_COMPONENTS + 'main-page/component-news',
     DIR_NAME_COMPONENTS + 'main-page/footer',
     DIR_NAME_COMPONENTS + 'main-page/header',
+    DIR_NAME_COMPONENTS + 'main-page/list-news',
 ];
