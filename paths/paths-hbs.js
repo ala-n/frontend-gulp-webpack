406ccc8b00b64f2188674770631bb39a821cb3d4
yadamska <yuli.adamskaya@gmail.com>
2018-10-02 11:28:42 +0300

    ts-config options and css

--- paths/paths-hbs.js
+++ paths/paths-hbs.js
@@ -6,4 +6,5 @@ module.exports.pathsArray = [
     DIR_NAME_COMPONENTS + 'main-page/footer',
     DIR_NAME_COMPONENTS + 'main-page/header',
     DIR_NAME_COMPONENTS + 'main-page/list-news',
+    DIR_NAME_COMPONENTS + 'news-page/information-news'
 ];
