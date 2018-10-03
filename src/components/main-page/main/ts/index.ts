c83ca792c345bc83c3ec655781e5bb5ac214a873
yadamska <yuli.adamskaya@gmail.com>
2018-10-03 14:01:39 +0300

    slider

--- src/components/main-page/main/ts/index.ts
+++ src/components/main-page/main/ts/index.ts
@@ -1,31 +1,58 @@
-function hideAllNews() {
-    let newsList : any = document.querySelectorAll('#list-news>article');
-    for (let i = 0; i < newsList.length; ++i) {
-        newsList[i].style.display = 'none';
+// function hideAllNews() {
+//     let newsList : any = document.querySelectorAll('#list-news>article');
+//     for (let i = 0; i < newsList.length; ++i) {
+//         newsList[i].style.display = 'none';
+//     }
+// }
+//
+// function showNews1() {
+//     hideAllNews();
+//     let news1 = document.getElementById('news-1');
+//     news1.style.display = "block";
+//
+// }
+//
+// function showNews2() {
+//     hideAllNews();
+//     let news2 = document.getElementById('news-2');
+//     news2.style.display = "block";
+//
+// }
+//
+// function showNews3() {
+//     hideAllNews();
+//     let news3 = document.getElementById('news-3');
+//     news3.style.display = "block";
+//
+// }
+//
+// document.querySelector('.button-chose-3').addEventListener('click', showNews3);
+// document.querySelector('.button-chose-2').addEventListener('click', showNews2);
+// document.querySelector('.button-chose-1').addEventListener('click', showNews1);
+//
+// document.querySelector('.arrow prev').addEventListener('click', showNews2);
+(function () {
+    const next:any = document.querySelector('.arrow-next-slide');
+    console.log(next, 3);
+
+    next.onclick = () => {
+        goToSlide(currentSlide + 1);
+    };
+
+    let slides:any = document.querySelectorAll('.carousel-items .carousel-item');
+
+    let currentSlide = 0;
+    goToSlide(1);
+
+    function goToSlide(n:number) {
+        slides[currentSlide].className = 'carousel-item';
+        currentSlide = (n + slides.length) % slides.length;
+        slides[currentSlide].className = 'carousel-item showing';
     }
-}
 
-function showNews1() {
-    hideAllNews();
-    let news1 = document.getElementById('news-1');
-    news1.style.display = "block";
+    const previous:any = document.querySelector('.arrow-previous-slide');
 
-}
-
-function showNews2() {
-    hideAllNews();
-    let news2 = document.getElementById('news-2');
-    news2.style.display = "block";
-
-}
-
-function showNews3() {
-    hideAllNews();
-    let news3 = document.getElementById('news-3');
-    news3.style.display = "block";
-
-}
-
-document.querySelector('.button-chose-3').addEventListener('click', showNews3);
-document.querySelector('.button-chose-2').addEventListener('click', showNews2);
-document.querySelector('.button-chose-1').addEventListener('click', showNews1);
+    previous.onclick = function () {
+        goToSlide(currentSlide - 1);
+    };
+}());
