61c3524d2861ce8957723bcb22b40ba09c05c32e
yadamska <yuli.adamskaya@gmail.com>
2018-10-08 11:24:43 +0300

    slider(ts)

--- src/components/main-page/list-news/ts/index.ts
+++ src/components/main-page/list-news/ts/index.ts
@@ -1,44 +1,45 @@
 // (function () {
-function getNumCurrentSlide(slider: HTMLDivElement): number {
-    const activeDot: HTMLDivElement = slider.querySelector('.active');
-    return +activeDot.title - 1;
+function getNumCurrentSlide(lengthSlidesList: number, activeDot: number): number {
+    return lengthSlidesList-activeDot;
 }
 
-function getNewCurrentSlide(showing: string, numCurrentSlide: number, lengthSlidesList: number, nextDot: number): number {
+function getNewCurrentSlide(showing: string, numCurrentSlide: number, lengthSlidesList: number, activeDot: number): number {
     let nextSlide: number = 0;
     switch (showing) {
         case 'prev':
-            nextSlide = numCurrentSlide - 1;
-            break;
-        case 'next':
             nextSlide = numCurrentSlide + 1;
             break;
+        case 'next':
+            nextSlide = numCurrentSlide  - 1;
+            break;
         case 'dot':
-            if(nextDot) {
-                nextSlide = +nextDot - 1;
+            if(activeDot) {
+                nextSlide = lengthSlidesList-activeDot;
             }
             break;
     }
+    console.log((nextSlide + lengthSlidesList) % lengthSlidesList);
     return (nextSlide + lengthSlidesList) % lengthSlidesList;
 }
 
-function showNewCurrentSlide(currentSlide: number, slides: NodeListOf<Element>, dots: NodeListOf<Element>): void {
+function showNewCurrentSlide(currentSlide: number, slides: NodeListOf<Element>, dots: NodeListOf<Element>, lengthSlidesList: number): void {
     slides[currentSlide].classList.add('show-slide');
-    dots[currentSlide].classList.add('active');
+    dots[lengthSlidesList - currentSlide - 1].classList.add('active');
 }
 
-function hideCurrentSlide(numCurrentSlide: number, slides: NodeListOf<Element>, dots: NodeListOf<Element>): void {
+function hideCurrentSlide(numCurrentSlide: number, slides: NodeListOf<Element>, dots: NodeListOf<Element>, activeDot: number): void {
     slides[numCurrentSlide].classList.remove('show-slide');
-    dots[numCurrentSlide].classList.remove('active');
+    dots[activeDot - 1].classList.remove('active');
 }
 
 function getSlide(event: any, showing: string): void {
     const slider: HTMLDivElement = event.target.closest('.slider');
-    let numCurrentSlide: number = getNumCurrentSlide(slider);
+    const activeDot: HTMLDivElement = slider.querySelector('.active');
     const slides: NodeListOf<Element> = slider.querySelectorAll('.information');
     const dots: NodeListOf<Element> = slider.querySelectorAll('.dot');
-    hideCurrentSlide(numCurrentSlide, slides, dots);
-    showNewCurrentSlide(getNewCurrentSlide(showing, numCurrentSlide, slides.length, event.target.title), slides, dots);
+    let numCurrentSlide: number = getNumCurrentSlide(slides.length, +activeDot.title);
+    hideCurrentSlide(numCurrentSlide, slides, dots,  +activeDot.title);
+    showNewCurrentSlide(getNewCurrentSlide(showing, numCurrentSlide, slides.length, event.target.title), slides, dots, slides.length);
 }
 
 
