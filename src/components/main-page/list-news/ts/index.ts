1087286e4cfa43652bfb0de44284d4c53fc19565
yadamska <yuli.adamskaya@gmail.com>
2018-10-10 14:03:36 +0300

    - carousel class

--- src/components/main-page/list-news/ts/index.ts
+++ src/components/main-page/list-news/ts/index.ts
@@ -1,73 +1,92 @@
-// (function () {
-function getNumCurrentSlide(lengthSlidesList: number, activeDot: number): number {
-    return lengthSlidesList - activeDot;
-}
+class Carousel extends HTMLElement {
 
-function getNewCurrentSlide(showing: string, numCurrentSlide: number, lengthSlidesList: number, activeDot: number): number {
-    let nextSlide: number = 0;
-    switch (showing) {
-        case 'prev':
-            nextSlide = numCurrentSlide + 1;
-            break;
-        case 'next':
-            nextSlide = numCurrentSlide - 1;
-            break;
-        case 'dot':
-            nextSlide = lengthSlidesList - activeDot;
-            break;
+    constructor() {
+        super();
     }
-    return (nextSlide + lengthSlidesList) % lengthSlidesList;
-}
 
-function showNewCurrentSlide(currentSlide: number, slides: NodeListOf<Element>, dots: NodeListOf<Element>, lengthSlidesList: number): void {
-    slides[currentSlide].classList.add('show-slide');
-    dots[lengthSlidesList - currentSlide - 1].classList.add('active');
-}
+    connectedCallback() {
+        this.bindEvents();
+    }
 
-function hideCurrentSlide(numCurrentSlide: number, slides: NodeListOf<Element>, dots: NodeListOf<Element>, activeDot: number): void {
-    slides[numCurrentSlide].classList.remove('show-slide');
-    dots[activeDot - 1].classList.remove('active');
-}
+    // Simple get
+    get value() {
+        return this.getAttribute('value');
+    }
+    set value(val) {
+        this.setAttribute('value', val);
+    }
 
-function getSlide(event: any, showing: string): void {
-    const slider: HTMLDivElement = event.target.closest('.slider');
-    const slides: NodeListOf<Element> = slider.querySelectorAll('.information');
-    const dots: NodeListOf<Element> = slider.querySelectorAll('.dot');
-    const activeDot: HTMLDivElement = slider.querySelector('.active');
-    const numCurrentSlide: number = getNumCurrentSlide(slides.length, +activeDot.title);
-    hideCurrentSlide(numCurrentSlide, slides, dots, +activeDot.title);
-    showNewCurrentSlide(getNewCurrentSlide(showing, numCurrentSlide, slides.length, event.target.title), slides, dots, slides.length);
-}
+    bindEvents() {
+        this.clickPreviousSlide();
+        this.clickNextSlide();
+        this. clickDot();
+    }
+    getNumCurrentSlide(lengthSlidesList: number, activeDot: number): number {
+        return lengthSlidesList - activeDot;
+    }
+
+    getNewCurrentSlide(showing: string, numCurrentSlide: number, lengthSlidesList: number, activeDot: number): number {
+        let nextSlide: number = 0;
+        switch (showing) {
+            case 'prev':
+                nextSlide = numCurrentSlide + 1;
+                break;
+            case 'next':
+                nextSlide = numCurrentSlide - 1;
+                break;
+            case 'dot':
+                nextSlide = lengthSlidesList - activeDot;
+                break;
+        }
+        return (nextSlide + lengthSlidesList) % lengthSlidesList;
+    }
+
+    showNewCurrentSlide(currentSlide: number, slides: NodeListOf<Element>, dots: NodeListOf<Element>, lengthSlidesList: number): void {
+        slides[currentSlide].classList.add('show-slide');
+        dots[lengthSlidesList - currentSlide - 1].classList.add('active');
+    }
+
+    hideCurrentSlide(numCurrentSlide: number, slides: NodeListOf<Element>, dots: NodeListOf<Element>, activeDot: number): void {
+        slides[numCurrentSlide].classList.remove('show-slide');
+        dots[activeDot - 1].classList.remove('active');
+    }
+
+    getSlide(event: any, showing: string): void {
+        const slider: HTMLDivElement = event.target.closest('.slider');
+        const slides: NodeListOf<Element> = slider.querySelectorAll('.information');
+        const dots: NodeListOf<Element> = slider.querySelectorAll('.dot');
+        const activeDot: HTMLDivElement = slider.querySelector('.active');
+        const numCurrentSlide: number = this.getNumCurrentSlide(slides.length, +activeDot.title);
+        this.hideCurrentSlide(numCurrentSlide, slides, dots, +activeDot.title);
+        this.showNewCurrentSlide( this.getNewCurrentSlide(showing, numCurrentSlide, slides.length, event.target.title), slides, dots, slides.length);
+    }
 
 
-function clickPreviousSlide() {
-    const previousArrows = document.querySelectorAll('.previous');
-    for (let index = 0; index < previousArrows.length; index++) {
-        previousArrows[index].addEventListener('click', (event: any) => {
-            getSlide(event, 'prev');
-        })
+    clickPreviousSlide() {
+        const previousArrows = document.querySelectorAll('.previous');
+        for (let index = 0; index < previousArrows.length; index++) {
+            previousArrows[index].addEventListener('click', (event: any) => {
+                this.getSlide(event, 'prev');
+            })
+        }
+    }
+
+    clickNextSlide() {
+        const nextArrows = document.querySelectorAll('.next');
+        for (let index = 0; index < nextArrows.length; index++) {
+            nextArrows[index].addEventListener('click', (event: any) => {
+                this.getSlide(event, 'next');
+            })
+        }
+    }
+
+    clickDot() {
+        const dotsArray = document.querySelectorAll('.dots-wrapper');
+        for (let index = 0; index < dotsArray.length; index++) {
+            dotsArray[index].addEventListener('click', (event: any) => {
+                this.getSlide(event, 'dot');
+            });
+        }
     }
 }
-
-function clickNextSlide() {
-    const nextArrows = document.querySelectorAll('.next');
-    for (let index = 0; index < nextArrows.length; index++) {
-        nextArrows[index].addEventListener('click', (event: any) => {
-            getSlide(event, 'next');
-        })
-    }
-}
-
-function clickDot() {
-    const dotsArray = document.querySelectorAll('.dots-wrapper');
-    for (let index = 0; index < dotsArray.length; index++) {
-        dotsArray[index].addEventListener('click', (event: any) => {
-            getSlide(event, 'dot');
-        });
-    }
-}
-
-clickPreviousSlide();
-clickNextSlide();
-clickDot();
-// }());
+customElements.define('slide-carousel', Carousel);
