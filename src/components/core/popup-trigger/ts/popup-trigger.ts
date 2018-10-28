092e95efe5e28fc0c5525fe612c2257efc5cdd52
Yuliya Adamskaya <yuli.adamskaya@gmail.com>
2018-10-28 11:00:02 +0300

    custom element popup menu

--- src/components/core/popup-trigger/ts/popup-trigger.ts
+++ src/components/core/popup-trigger/ts/popup-trigger.ts
@@ -1,53 +1,43 @@
 import PopupMenu from './popup-menu';
 
 class PopupTrigger extends HTMLElement {
-	private _parent: PopupMenu;
+    private _popup_menu: PopupMenu;
 
-	constructor() {
-		super();
-	}
+    constructor() {
+        super();
+    }
 
-	_onUpdate = () => this.rerender();
+    get newActiveValue(): string {
+        return this.getAttribute('data-popup-trigger');
+    }
 
-	_onClick = (event: MouseEvent) => {
-		console.log(this._parent.elements);
-		// const target = event.target as HTMLElement;
-		// const attrValue = target.getAttribute('data-popup-trigger');
-		// console.log(attrValue);
-		// if (attrValue) {
-		// 	// this.setActive(attrValue);
-		// 	event.stopPropagation();
-		// 	event.preventDefault();
-		// }
-	};
+    _onClick = (event: MouseEvent) => {
+        const target = event.target as HTMLElement;
+        target.closest('.nav-menu').querySelector('.popup-menu').classList.toggle('hide');
+        event.stopPropagation();
+        event.preventDefault();
+    };
 
-	bindEvents() {
-		this.addEventListener('click', this._onClick);
-	}
+    _onUpdate = () => this.rerender();
 
-	private connectedCallback() {
-		this._parent = this.closest(PopupMenu.is) as PopupMenu;
+    connectedCallback() {
+        this._popup_menu = this.closest('.nav-menu').querySelector(PopupMenu.is) as PopupMenu;
+        this.bindEvents();
+        this._popup_menu.addEventListener('sc-elem-changed', this._onUpdate);
+    }
 
-		console.log(this._parent);
-		this.rerender();
-		this.bindEvents();
-		this._parent.addEventListener('sc-elem-changed', this._onUpdate);
-	}
+    bindEvents() {
+        this.addEventListener('click', this._onClick);
+    }
 
-	private disconnectedCallback() {
-		this._parent.removeEventListener('sc-elem-changed', this._onUpdate);
-	}
+    private disconnectedCallback() {
+        this._popup_menu.removeEventListener('sc-elem-changed', this._onUpdate);
+    }
 
-	private rerender() {
-		const html = '';
-		const count = this._parent.elements.length;
-		const activeIndex = this._parent.activeIndex;
-		console.log(activeIndex);
-		for (let i = 0; i < count; ++i) {
-			// html += this.buildDot(i, i === activeIndex);
-		}
-		this.innerHTML = html;
-	}
+    private rerender() {
+        this.setAttribute('data-popup-trigger', this._popup_menu.value);
+        this.querySelector('.language-change').innerHTML = this.newActiveValue;
+    }
 }
 
 customElements.define('popup-trigger', PopupTrigger);
